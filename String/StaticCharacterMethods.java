9eea5b4b74de2767b088e101f277e1068ba91de7
Fabien Thiombiano <thiombianofabien@gmail.com>
2017-01-11 00:54:14 -0600

    fixed typos

--- String/StaticCharacterMethods.java
+++ String/StaticCharacterMethods.java
@@ -17,15 +17,15 @@ public class StaticCharacterMethods
 		
 		//display character info
 		System.out.printf("Character is defined: %b%n", Character.isDefined(ch)); //determines if character is defined in unicode
-		System.out.printf("Characte is digit: %b%n", Character.isDigit(ch));
-		System.out.printf("Characte is first character in a Java identifier: %b%n", Character.isJavaIdentifierStart((ch)));
-		System.out.printf("Characte is part of a Java identifier: %b%n", Character.isJavaIdentifierPart(ch));
-		System.out.printf("Characte is a letter: %b%n", Character.isLetter(ch));
-		System.out.printf("Characte is a letter or a digit: %b%n", Character.isLetterOrDigit(ch));
-		System.out.printf("Characte is lowerCase: %b%n", Character.isLowerCase(ch));
-		System.out.printf("Characte is upperCase: %b%n", Character.isUpperCase(ch));
-		System.out.printf("Characte toLower: %s%n", Character.toLowerCase(ch));
-		System.out.printf("Characte toUpper: %s%n", Character.toUpperCase(ch));
+		System.out.printf("Character is digit: %b%n", Character.isDigit(ch));
+		System.out.printf("Character is first character in a Java identifier: %b%n", Character.isJavaIdentifierStart((ch)));
+		System.out.printf("Character is part of a Java identifier: %b%n", Character.isJavaIdentifierPart(ch));
+		System.out.printf("Character is a letter: %b%n", Character.isLetter(ch));
+		System.out.printf("Character is a letter or a digit: %b%n", Character.isLetterOrDigit(ch));
+		System.out.printf("Character is lowerCase: %b%n", Character.isLowerCase(ch));
+		System.out.printf("Character is upperCase: %b%n", Character.isUpperCase(ch));
+		System.out.printf("Character toLower: %s%n", Character.toLowerCase(ch));
+		System.out.printf("Character toUpper: %s%n", Character.toUpperCase(ch));
 		
 	}
 
